f512735057c79d70010194797d8700afb3fe43fd
Angel Carrasco <acarrascocesa@gmail.com>
2023-04-13 12:45:28 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import Movie from "./pages/Movie/Movie";
+import MovieList from "./components/movieList/movieList";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="movies/:type" element={<MovieList />} />
+          <Route path="/*" element={<h1>Movie not found!!!</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
